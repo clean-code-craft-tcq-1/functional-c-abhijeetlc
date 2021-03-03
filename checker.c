37a89604f9eda700f09faed20ab954df689685de
Abhijeet Chitagubbi <77390129+abhijeetlc@users.noreply.github.com>
2021-03-03 17:38:40 +0530

    BMS_Check
    Updated logic

--- checker.c
+++ checker.c
@@ -1,59 +1,85 @@
+/* Author : Abhijeet C */
+/* Program : Primitive BMS simulation */
+
+/* Includes */
 #include <stdio.h>
 #include <assert.h>
 #include <stdlib.h>
 #include <math.h>
 #include <stdbool.h>
 
+/* defines */
 
-#define Mintemp		0
-#define maxtemp		45
+#define MAXCHARGERATE 0.8
+#define MAXTEMP 45
+#define MINTEMP 0
+#define MAXSOC 80
+#define MINSOC 20
 
-double temperaturecheck(float a)
+
+/* Check for temperature range */
+/* Arguement : Temperature  */
+int checktemperature(float TempIn)
 {
-	return( (a + fabs(a))/2);	 // Trying a pure funtion
+	int TempValue = ((MINTEMP < TempIn) || (TempIn < MAXTEMP))
+	if (TempValue)
+  {
+    printf(" The current Battery temperature is %f. \n", TempIn);
+    return 1;
+  } 
+  printf("Battery Temperature is %f. Out of range!\n", TempIn);
+  return 0;
+
 }
 
-bool evaluateTemp (double Temp)
+/* Check for Charge rate */
+/* Arguement : ChargeRate  */
+int CheckChargeRate(float chargeRate)
 {
-	bool result;
-	
-	if (Temp < Mintemp || Temp > maxtemp) {
-		 result = false;
-	}
+	int chargerate_check=(charge_rate>MAXCHARGERATE);
+	 if(chargerate_check)
+	   {
+		printf("Charge Rate is %f. Out of range!\n", chargeRate);
+		return 0;
+	   }
+	printf("Charge Rate is %f . OK \n", chargeRate);  
+	return 1;
 }
 
-int batteryIsOk(float temperature, float soc, float chargeRate) {
-  
-  double TempertureValue = temperaturecheck(temperature);
-  bool result;	
-  
- //Check for temperature out of range 
-  if(!(result=evaluateTemp(TempertureValue))) {
-    printf("Temperature Not OK !\n");
-    return 0;
-  } 
-  
-  
-   if(soc < 20 ) {
-    printf("State of Charge Low!\n");
-    return 0;
-  } 
-
-    if(soc > 80 ) {
-    printf("State of Charge High!\n");
-    return 0;
-  } 
-  
-  if(chargeRate > 0.8) {
-    printf("Charge Rate out of range!\n");
-    return 0;
+/* Check for SOC status */
+ /* Arguement : SOC  */
+int Checksoc(float soc)
+{
+  int socValue= ((soc < MINSOC) || (soc > MAXSOC));
+  if (socValue)
+  {
+     printf("State of Charge is %f percent.Out of range!\n", soc);
+	 
+     return 0;
   }
-	
-  printf(" Battery OK :) !\n");
   return 1;
 }
 
+/* Battery overall check */
+/* Arguement : Temperature  , SOC , Chanrgerate */
+int checkBattery(float TempIn, float soc, float chargeRate) {
+  
+  int BatteryState = 0;
+  
+  int socstatus,temperaturecheck,chargeratecheck,BatteryState;
+  
+     socstatus = Checksoc(soc);
+     chargeratecheck = CheckChargeRate(chargeRate);
+     temperaturecheck = checktemperature(TempIn);
+     BatteryState = (socstatus & chargeratecheck & temperaturecheck);
+     return (BatteryState);
+ 
+}
+
+
+/*  Main function to call test cases  */
+
 int main() {
-  assert(batteryIsOk(25, 70, 0.7));
-  assert(!batteryIsOk(50, 85, 0));
+  assert(checkBattery(25, 70, 0.7));
+  assert(!checkBattery(50, 85, 0));
 }
